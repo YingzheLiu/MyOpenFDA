9fe65cb4c3e781654a68d530578bd14466b7d4e2
Nikki <yingzhe@usc.edu>
2020-11-08 16:27:03 -0800

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import SearchForm from "./SearchForm";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
 import Favorites from "./Favorites";
 import FavoriteDetail from "./FavoriteDetail";
-import { fatchFavorites, fetchFavorites } from "./api";
 
 function App() {
-  // const favorites = [
-  //   {
-  //     id: 0,
-  //     drugName: "remicade",
-  //     adverseEvent: "drug ineffective",
-  //     numOfDrugReport: 117505,
-  //     numOfAdverseEventReport: 768745,
-  //     numOfDrugAndAEReport: 16464,
-  //     numOfTotalReport: 12254197,
-  //     dateTime: "08/11/2020 11:22:20",
-  //   },
-  //   {
-  //     id: 1,
-  //     drugName: "inflectra",
-  //     adverseEvent: "infection",
-  //     numOfDrugReport: 6094,
-  //     numOfAdverseEventReport: 80383,
-  //     numOfDrugAndAEReport: 117,
-  //     numOfTotalReport: 12254197,
-  //     dateTime: "08/10/2020 11:22:20",
-  //   },
-  // ];
-
   return (
     <Router>
       <div className="container mt-3">
