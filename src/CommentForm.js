351d886b3e3be33dd2e5a6e7d9718cd49ca4a536
Nikki <yingzhe@usc.edu>
2020-11-15 17:08:32 -0800

    add validation to a form

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import moment from "moment";
+import Form from "react-bootstrap/Form";
 
 export default function CommentForm({ createComment }) {
   const [author, setAuthor] = useState("");
   const [text, setText] = useState("");
+  const [validated, setValidated] = useState(false);
 
   function handleAuthorChange(event) {
     setAuthor(event.target.value);
@@ -12,36 +14,51 @@ export default function CommentForm({ createComment }) {
     setText(event.target.value);
   }
   function handleSubmit(event) {
+    const form = event.currentTarget;
     event.preventDefault();
-    var currDateTime = moment().format("DD/MM/YYYY HH:mm:ss");
-    createComment(author, text, currDateTime);
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      setValidated(true);
+      var currDateTime = moment().format("DD/MM/YYYY HH:mm:ss");
+      createComment(author, text, currDateTime);
+    }
   }
   return (
     <>
       <div className="mt-3 mb-3">
         <h4>Add Comment</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="author">Author</label>
-            <input
+        <Form noValidate onSubmit={handleSubmit} validated={validated}>
+          <Form.Group>
+            <Form.Label htmlFor="author">Author</Form.Label>
+            <Form.Control
               type="text"
-              className="form-control"
-              id="author"
               value={author}
               onChange={handleAuthorChange}
-              data-testid="author"
-            ></input>
-          </div>
-          <div className="form-group">
-            <label htmlFor="text">Comment</label>
-            <textarea
               className="form-control"
-              id="text"
+              id="author"
+              data-testid="author"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your name.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="adverseEvent">Comment</Form.Label>
+            <Form.Control
+              type="textarea"
               value={text}
               onChange={handleTextChange}
+              className="form-control"
+              id="text"
               data-testid="text"
-            ></textarea>
-          </div>
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter your comment.
+            </Form.Control.Feedback>
+          </Form.Group>
           <button
             className="btn btn-primary"
             type="submit"
@@ -49,7 +66,7 @@ export default function CommentForm({ createComment }) {
           >
             Post
           </button>
-        </form>
+        </Form>
       </div>
     </>
   );
